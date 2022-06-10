cc4f3099bdc32548408eee3f358af41996636b4b
shono <>
2022-06-10 10:59:29 -0700

    Remove once

--- vision_model/__init__.py
+++
@@ -1,5 +0,0 @@
-from .model_base import ModelBase
-from .plugin_base import PluginBase
-from .trainer_callback_interface import TrainerCallbackInterface
-
-__all__ = ['ModelBase', 'PluginBase', 'TrainerCallbackInterface']
